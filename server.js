f63582c3e1c6db502399c7ef746be777cf723e36
Amogh Kumar <amoghku02@gmail.com>
2020-12-30 07:47:31 +0530

    created routes

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 5000 ;
+
 app.get('/', (req,res) => res.send('API running!'));
 
-const PORT = process.env.PORT || 5000 ;
+//defining routes
+app.use('/api/auth', require('./routes/api/auth.js'));
+app.use('/api/posts', require('./routes/api/posts.js'));
+app.use('/api/profile', require('./routes/api/profile.js'));
+app.use('/api/users', require('./routes/api/users.js'));
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
