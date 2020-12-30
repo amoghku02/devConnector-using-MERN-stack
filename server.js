66e1d393a1a790842abe9bbba67f3704edf3fa94
Amogh Kumar <amoghku02@gmail.com>
2020-12-30 19:47:12 +0530

    changes made

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
 const PORT = process.env.PORT || 5000 ;
 
 app.get('/', (req,res) => res.send('API running!'));
+
+//Init
+app.use(express.json({extended: false}));
 
 //defining routes
 app.use('/api/auth', require('./routes/api/auth.js'));
